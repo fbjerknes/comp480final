454685bb3a5ebe590cb8155af51d80822020f7fa
Finn Bjerknes <fmb1@rice.edu>
2021-05-11 22:50:33 -0500

    upa

--- supernode_creation.py
+++ supernode_creation.py
@@ -233,6 +233,50 @@ def erdos_renyi(n, p):
     return g
 
 
+def total_degree(g):
+    """
+    Compute total degree of the undirected graph g.
+
+    Arguments:
+    g -- undirected graph
+
+    Returns:
+    Total degree of all nodes in g
+    """
+    return sum(map(len, g.values()))
+
+
+def upa(n, m):
+    """
+    Generate an undirected graph with n node and m edges per node
+    using the preferential attachment algorithm.
+
+    Arguments:
+    n -- number of nodes
+    m -- number of edges per node
+
+    Returns:
+    undirected random graph in UPAG(n, m)
+    """
+    g = {}
+    if m <= n:
+        g = make_complete_graph(m)
+        for new_node in range(m, n):
+            # Find <=m nodes to attach to new_node
+            totdeg = float(total_degree(g))
+            nodes = g.keys()
+            probs = []
+            for node in nodes:
+                probs.append(len(g[node]) / totdeg)
+            mult = distinct_multinomial(m, probs)
+
+            # Add new_node and its random neighbors
+            g[new_node] = set()
+            for idx in mult:
+                node = nodes[idx]
+                g[new_node].add(node)
+                g[node].add(new_node)
+    return g
 
 
 def make_complete_graph(num_nodes):
@@ -259,6 +303,28 @@ def make_complete_graph(num_nodes):
     return result
 
 
+def distinct_multinomial(ntrials, probs):
+    """
+    Draw ntrials samples from a multinomial distribution given by
+    probs.  Return a list of indices into probs for all distinct
+    elements that were selected.  Always returns a list with between 1
+    and ntrials elements.
+
+    Arguments:
+    ntrials -- number of trials
+    probs   -- probability vector for the multinomial, must sum to 1
+
+    Returns:
+    A list of indices into probs for each element that was chosen one
+    or more times.  If an element was chosen more than once, it will
+    only appear once in the result.
+    """
+    ### select ntrials elements randomly
+    mult = numpy.random.multinomial(ntrials, probs)
+
+    ### turn the results into a list of indices without duplicates
+    result = [i for i, v in enumerate(mult) if v > 0]
+    return result
 
 
 g1 = erdos_renyi(20, 0.14)
