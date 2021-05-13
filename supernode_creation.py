bfd58bba11b16d14716058e4c4cd509bc68ff934
Alex Berlaga <awb4@rice.edu>
2021-05-12 19:13:07 -0500

    half commented

--- supernode_creation.py
+++ supernode_creation.py
@@ -13,10 +13,11 @@ import time
 import copy
 import networkx as nx
 
-NODES_MERGED_THRESHOLD = 100
-EDR_THRESHOLD = 0.1
+NODES_MERGED_THRESHOLD = 200
+EDR_THRESHOLD_ERDOS = 0.1
+EDR_THRESHOLD_CLUSTERED = 0.2
 
-TIME_SERIES_NODES_MERGED_THRESHOLD = 30
+TIME_SERIES_NODES_MERGED_THRESHOLD = 200
 
 NUM_DATA_POINTS = 30
 
@@ -52,52 +53,22 @@ class GraphVisualization:
 
 # Driver code
 
-def minhash(set_a, set_b, hashes, seed):
-    hashedset = []
-    for elem in set_a:
-        hashedset.append(hash_func(2**32, seed)(elem))
-    return min(hashedset)
-
-class SuperNodeCreator:
-    def __init__(self, graph, num_hashes, compression_factor):
-        self.graph = graph
-        self.num_hashes = num_hashes
-        self.compression_factor = compression_factor
-    def insert_node(self, node):
-        pass
-    def delete_node(self, node):
-        pass
-    def nodes(self):
-        pass
-    def super_nodes(self):
-        pass
-    def edges(self):
-        pass
-    def super_edges(self):
-        pass
-    def get_neighbors(self, node):
-        pass
-    def get_supernode(self, node):
-        pass
-    def get_subnodes(self, supernode):
-        pass
-
-def lsh(set_a):
-    return 0
-
-
-def compress(graph):
-    newgraph = {}
-    for i in graph.keys():
-        newgraph[i] = lsh(graph[i])
-
 
 def genhash():
+    """
+    :return: a random hash function with the values being any unsigned int.
+    """
     seed = random.randint(0, 2**32-1)
     return lambda x: murmurhash3_32(x, seed=seed)
 
 
 def produce_hash(k, l):
+    """
+    Create l hash tables with k hash functions per table.
+    :param k: number of hash functions per table
+    :param l: number of hash tables
+    :return: the list of hash tables
+    """
     h = []
     for i in range(l):
         n = []
@@ -108,31 +79,57 @@ def produce_hash(k, l):
 
 
 def minhash(A, m, hashes):
+    """
+    Computes m minhash values of any set
+    :param A: Set to be hashed
+    :param m: number of hash funcitons -- must be <= len(hashes)
+    :param hashes: hash functions to pull from
+    :return: A list of minhash values for the set A
+    """
     hashvalues = []
+    #Iterate over every hash function
     for i in range(m):
         cur = hashes[i]
         vals = []
         for j in A:
+            #Append a hash value of every set to a list
             vals.append(cur(j))
+        #If there are no hash values, return 0, otherwise return the minimum of the hash values, hence minhash.
         hashvalues.append(0 if len(vals) == 0 else min(vals))
+    #return the set of all minhash values for every hash function
     return hashvalues
 
-
-
 class HashTable:
+    """
+    Class for a table to store LSH values
+    """
     def __init__(self, K, L, R):
+        """
+        Create a set of tables of hash values
+        :param K: number of hash functions per table
+        :param L: number of hash tables
+        :param R: range of hash functions
+        """
         self.k = K
         self.l = L
         self.r = R
+        #hash tables
         self.tables = []
         for i in range(self.l):
             self.tables.append(defaultdict(list))
+        #random linear combination constants for hash bucket storage.
         self.a = []
         for i in range(self.k + 1):
             self.a.append(random.randint(0, 2**32 - 1))
 
     def insert(self, hashcodes, id):
+        """
+        Stores an item in the hash table
+        :param hashcodes: list of lists of hash values
+        :param id: ID of item to store
+        """
         for i in range(self.l):
+            #uses the hashcodes to find a bucket for each table.
             bucket = self.a[-1]
             for j in range(self.k):
                 bucket += hashcodes[i][j] * self.a[j]
@@ -140,11 +137,19 @@ class HashTable:
             self.tables[i][bucket].append(id)
 
     def lookup(self, hashcodes):
+        """
+        Searches through the hash table for a list of hash codes, computes the intersection of all
+        values that match at least one of the buckets determined by the hash codes
+        :param hashcodes: list of all hashcodes to attempt to match
+        :return: values that fit in one or more of the desired buckets
+        """
+        #find a bucket for every hash table
         bucket = self.a[-1]
         for j in range(self.k):
             bucket += hashcodes[0][j] * self.a[j]
         bucket = (bucket % (2 ** 31 - 1)) % self.r
         idset = set(self.tables[0][bucket])
+        #Merge the set of all values in this bucket to the set of IDs to return.
         for i in range(1, self.l):
             bucket = self.a[-1]
             for j in range(self.k):
@@ -286,91 +291,6 @@ def total_degree(g):
     return sum(map(len, g.values()))
 
 
-def upa(n, m):
-    """
-    Generate an undirected graph with n node and m edges per node
-    using the preferential attachment algorithm.
-
-    Arguments:
-    n -- number of nodes
-    m -- number of edges per node
-
-    Returns:
-    undirected random graph in UPAG(n, m)
-    """
-    g = {}
-    if m <= n:
-        g = make_complete_graph(m)
-        for new_node in range(m, n):
-            # Find <=m nodes to attach to new_node
-            totdeg = float(total_degree(g))
-            nodes = list(g.keys())
-            probs = []
-            for node in nodes:
-                probs.append(len(g[node]) / totdeg)
-            mult = distinct_multinomial(m, probs)
-
-            # Add new_node and its random neighbors
-            g[new_node] = {}
-            for idx in mult.keys():
-                node = nodes[idx]
-                g[new_node][node] = 1
-                g[node][new_node] = 1
-    return g
-
-
-def make_complete_graph(num_nodes):
-    """
-    Returns a complete graph containing num_nodes nodes.
-
-    The nodes of the returned graph will be 0...(num_nodes-1) if num_nodes-1 is positive.
-    An empty graph will be returned in all other cases.
-
-    Arguments:
-    num_nodes -- The number of nodes in the returned graph.
-
-    Returns:
-    A complete graph in dictionary form.
-    """
-    result = {}
-
-    for node_key in range(num_nodes):
-        result[node_key] = {}
-        for node_value in range(num_nodes):
-            if node_key != node_value:
-                result[node_key][node_value] = 1
-
-    return result
-
-
-def distinct_multinomial(ntrials, probs):
-    """
-    Draw ntrials samples from a multinomial distribution given by
-    probs.  Return a list of indices into probs for all distinct
-    elements that were selected.  Always returns a list with between 1
-    and ntrials elements.
-
-    Arguments:
-    ntrials -- number of trials
-    probs   -- probability vector for the multinomial, must sum to 1
-
-    Returns:
-    A list of indices into probs for each element that was chosen one
-    or more times.  If an element was chosen more than once, it will
-    only appear once in the result.
-    """
-    ### select ntrials elements randomly
-    mult = np.random.multinomial(ntrials, probs)
-
-    ### turn the results into a list of indices without duplicates
-    result = [i for i, v in enumerate(mult) if v > 0]
-    r = {}
-    for i in result:
-        r[i] = 1
-    return r
-
-
-
 def random_graph_generator(n):
     rg = defaultdict(lambda: defaultdict(int))
     for i in range(n):
@@ -391,7 +311,6 @@ def random_graph_generator(n):
 
 
 g1 = erdos_renyi(20, 0.14)
-#print(g1)
 g2 = {0: {2: 3, 3: 4, 4: 1}, 1: {2: 2, 3: 7, 4: 1, 5: 4}, 2: {0: 3, 1 : 2}, 3: {0: 4, 1 : 7}, 4: {0: 1, 1 : 1}, 5: {1 : 4}}
 
 k = 2
@@ -419,7 +338,7 @@ g9_merges = []
 for i in range(len(g9)):
     if i in g9.keys():
         g9_candidates = g9_candidates = get_candidates(g9, k, l, r, hg9, i, hf9)
-        nm9 += create_supernode(g9, i, EDR_THRESHOLD, g9_candidates, g9_merges)
+        nm9 += create_supernode(g9, i, EDR_THRESHOLD_ERDOS, g9_candidates, g9_merges)
     if nm9 > 3:
         break
 
@@ -449,7 +368,7 @@ def run_tests():
     for i in range(len(g3)):
         if i in g3.keys():
             g3_candidates = get_candidates(g3, k, l, r, hg3, i, hf3)
-            nodes_merged += create_supernode(g3, i, EDR_THRESHOLD, g3_candidates, g3_merges)
+            nodes_merged += create_supernode(g3, i, EDR_THRESHOLD_ERDOS, g3_candidates, g3_merges)
         if nodes_merged > NODES_MERGED_THRESHOLD:
             break
 
@@ -466,7 +385,7 @@ def run_tests():
     for i in range(len(g4)):
         if i in g4.keys():
             g4_candidates = list(g4.keys())
-            nm += create_supernode(g4, i, EDR_THRESHOLD, g4_candidates, g4_merges)
+            nm += create_supernode(g4, i, EDR_THRESHOLD_ERDOS, g4_candidates, g4_merges)
         if nm > nodes_merged:
             break
 
@@ -480,9 +399,9 @@ def run_tests():
     print("")
 
 
-    print("UPA TEST")
+    print("CLUSTERED TEST")
 
-    g6 = upa(10000, 100)
+    g6 = random_graph_generator(10000)
     g7 = copy.deepcopy(g6)
     g8 = copy.deepcopy(g6)
     time1 = time.time()
@@ -494,7 +413,7 @@ def run_tests():
     for i in range(len(g6)):
         if i in g6.keys():
             g6_candidates = get_candidates(g6, k, l, r, hg6, i, hf6)
-            nodes_merged += create_supernode(g6, i, EDR_THRESHOLD, g6_candidates, g6_merges)
+            nodes_merged += create_supernode(g6, i, EDR_THRESHOLD_CLUSTERED, g6_candidates, g6_merges)
         if nodes_merged > NODES_MERGED_THRESHOLD:
             break
 
@@ -509,7 +428,7 @@ def run_tests():
     for i in range(len(g7)):
         if i in g7.keys():
             g7_candidates = list(g7.keys())
-            nm += create_supernode(g7, i, EDR_THRESHOLD, g7_candidates, g7_merges)
+            nm += create_supernode(g7, i, EDR_THRESHOLD_CLUSTERED, g7_candidates, g7_merges)
         if nm > nodes_merged:
             break
 
@@ -542,7 +461,7 @@ def produce_time_series():
     for i in range(len(g3)):
         if i in g3.keys():
             g3_candidates = get_candidates(g3, k, l, r, hg3, i, hf3)
-            c = create_supernode(g3, i, EDR_THRESHOLD, g3_candidates, g3_merges)
+            c = create_supernode(g3, i, EDR_THRESHOLD_ERDOS, g3_candidates, g3_merges)
             nodes_merged += c
             for j in range(c):
                 er_simi.append(dist(g5, g3, g3_merges))
@@ -556,19 +475,21 @@ def produce_time_series():
     for i in range(len(g4)):
         if i in g4.keys():
             g4_candidates = list(g4.keys())
-            d = create_supernode(g4, i, EDR_THRESHOLD, g4_candidates, g4_merges)
+            d = create_supernode(g4, i, EDR_THRESHOLD_ERDOS, g4_candidates, g4_merges)
             nm += d
             for j in range(d):
                 bf_simi.append(dist(g5, g4, g4_merges))
         if nm > nodes_merged:
             break
 
-    return {'Nodes Removed': range(nodes_merged), 'With LSH': er_simi, 'Without LSH': bf_simi}
+    m = min(len(er_simi), len(bf_simi))
+    return {'Nodes Removed': range(nodes_merged), 'With LSH': er_simi[:m], 'Without LSH': bf_simi[:m]}
 
 
 def produce_plot_data():
     er_data = [[[], [], []], [[], [], []]]
     for a in range(NUM_DATA_POINTS):
+        print("a = " + str(a))
         g3 = erdos_renyi(10000, 0.001)
 
 
@@ -585,7 +506,7 @@ def produce_plot_data():
         for i in range(len(g3)):
             if i in g3.keys():
                 g3_candidates = get_candidates(g3, k, l, r, hg3, i, hf3)
-                nodes_merged += create_supernode(g3, i, EDR_THRESHOLD, g3_candidates, g3_merges)
+                nodes_merged += create_supernode(g3, i, EDR_THRESHOLD_ERDOS, g3_candidates, g3_merges)
             if nodes_merged > NODES_MERGED_THRESHOLD:
                 break
 
@@ -600,7 +521,7 @@ def produce_plot_data():
         for i in range(len(g4)):
             if i in g4.keys():
                 g4_candidates = list(g4.keys())
-                nm += create_supernode(g4, i, EDR_THRESHOLD, g4_candidates, g4_merges)
+                nm += create_supernode(g4, i, EDR_THRESHOLD_ERDOS, g4_candidates, g4_merges)
             if nm > nodes_merged:
                 break
 
@@ -621,90 +542,44 @@ def produce_plot_data():
     er_plot_differences = {'Time Difference': time_dif, 'Accuracy Difference': sim_dif}
 
 
-    upa_data = [[[], [], []], [[], [], []]]
-
-    g6 = upa(10000, 100)
-    g7 = copy.deepcopy(g6)
-    g8 = copy.deepcopy(g6)
-    time1 = time.time()
-    hg6, hf6 = hash_graph(k, l, r, g6)
-    nodes_merged = 0
-    g6_merges = []
-
-    for i in range(len(g6)):
-        if i in g6.keys():
-            g6_candidates = get_candidates(g6, k, l, r, hg6, i, hf6)
-            nodes_merged += create_supernode(g6, i, EDR_THRESHOLD, g6_candidates, g6_merges)
-        if nodes_merged > NODES_MERGED_THRESHOLD:
-            break
-
-    upa_data[0][0].append(nodes_merged)
-    time2 = time.time()
-    upa_data[0][1].append(time2 - time1)
-    time3 = time.time()
-    nm = 0
-
-    g7_merges = []
-    for i in range(len(g7)):
-        if i in g7.keys():
-            g7_candidates = list(g7.keys())
-            nm += create_supernode(g7, i, EDR_THRESHOLD, g7_candidates, g7_merges)
-        if nm > nodes_merged:
-            break
-
-    upa_data[1][0].append(nm)
-    time4 = time.time()
-    upa_data[1][1].append(time4 - time3)
-    upa_data[0][2].append(dist(g8, g6, g6_merges))
-    upa_data[1][2].append(dist(g8, g7, g7_merges))
-
-    time_dif = []
-    sim_dif = []
-    for i in range(len(upa_data[0][1])):
-        time_dif.append(float(upa_data[1][1][i]) / upa_data[0][1][i])
-        sim_dif.append(float(upa_data[0][2][i]) / upa_data[1][2][i])
-
-    upa_plot_time = {'Without LSH': upa_data[1][1], 'With LSH': upa_data[0][1]}
-    upa_plot_similarity = {'Without LSH': upa_data[1][2], 'With LSH': upa_data[0][2]}
-    upa_plot_differences = {'Time Difference': time_dif, 'Accuracy Difference': sim_dif}
-
-
     rand_data = [[[], [], []], [[], [], []]]
 
-    g9 = upa(10000, 100)
-    g10 = copy.deepcopy(g9)
-    g11 = copy.deepcopy(g9)
-    time1 = time.time()
-    hg9, hf9 = hash_graph(k, l, r, g9)
-    nodes_merged = 0
-    g9_merges = []
+    for a in range(NUM_DATA_POINTS):
+        print("a = " + str(a))
+        g9 = random_graph_generator(10000)
+        g10 = copy.deepcopy(g9)
+        g11 = copy.deepcopy(g9)
+        time1 = time.time()
+        hg9, hf9 = hash_graph(k, l, r, g9)
+        nodes_merged = 0
+        g9_merges = []
 
-    for i in range(len(g9)):
-        if i in g9.keys():
-            g9_candidates = get_candidates(g9, k, l, r, hg9, i, hf9)
-            nodes_merged += create_supernode(g9, i, EDR_THRESHOLD, g9_candidates, g9_merges)
-        if nodes_merged > NODES_MERGED_THRESHOLD:
-            break
+        for i in range(len(g9)):
+            if i in g9.keys():
+                g9_candidates = get_candidates(g9, k, l, r, hg9, i, hf9)
+                nodes_merged += create_supernode(g9, i, EDR_THRESHOLD_CLUSTERED, g9_candidates, g9_merges)
+            if nodes_merged > NODES_MERGED_THRESHOLD:
+                break
 
-    rand_data[0][0].append(nodes_merged)
-    time2 = time.time()
-    rand_data[0][1].append(time2 - time1)
-    time3 = time.time()
-    nm = 0
+        rand_data[0][0].append(nodes_merged)
+        time2 = time.time()
+        rand_data[0][1].append(time2 - time1)
+        time3 = time.time()
+        nm = 0
 
-    g10_merges = []
-    for i in range(len(g10)):
-        if i in g10.keys():
-            g10_candidates = list(g10.keys())
-            nm += create_supernode(g10, i, EDR_THRESHOLD, g10_candidates, g10_merges)
-        if nm > nodes_merged:
-            break
+        g10_merges = []
+        for i in range(len(g10)):
+            if i in g10.keys():
+                g10_candidates = list(g10.keys())
+                nm += create_supernode(g10, i, EDR_THRESHOLD_CLUSTERED, g10_candidates, g10_merges)
+            if nm > nodes_merged:
+                break
 
-    rand_data[1][0].append(nm)
-    time4 = time.time()
-    rand_data[1][1].append(time4 - time3)
-    rand_data[0][2].append(dist(g11, g9, g9_merges))
-    rand_data[1][2].append(dist(g11, g10, g10_merges))
+        rand_data[1][0].append(nm)
+        time4 = time.time()
+        rand_data[1][1].append(time4 - time3)
+        rand_data[0][2].append(dist(g11, g9, g9_merges))
+        rand_data[1][2].append(dist(g11, g10, g10_merges))
 
     time_dif = []
     sim_dif = []
@@ -716,23 +591,30 @@ def produce_plot_data():
     rand_plot_similarity = {'Without LSH': rand_data[1][2], 'With LSH': rand_data[0][2]}
     rand_plot_differences = {'Time Difference': time_dif, 'Accuracy Difference': sim_dif}
 
-    return er_plot_time, er_plot_similarity, er_plot_differences, upa_plot_time, upa_plot_similarity, upa_plot_differences, rand_plot_time, rand_plot_similarity, rand_plot_differences
+    return er_plot_time, er_plot_similarity, er_plot_differences, rand_plot_time, rand_plot_similarity, rand_plot_differences
 
 
 series = produce_time_series()
 data = produce_plot_data()
 
-actual = [pd.DataFrame(data=series)]
+actual = pd.DataFrame(data=series)
 
-for i in data:
-    actual.append(pd.DataFrame(data=i))
+actual.plot(x='Nodes Removed')
+plt.show()
 
-for i in range(10):
-    if i == 0:
-        actual[i].plot(x='Nodes Removed')
-    elif i % 3 == 0:
-        actual[i].plot(x='Time Difference')
+for i in range(6):
+    graph_num += 1
+    fig = plt.figure(graph_num)
+    if i % 3 == 2:
+        plt.scatter(x=data[i]['Time Difference'], y=data[i]['Accuracy Difference'])
+        ax = fig.add_subplot()
+        ax.set_xlabel('Times Faster with LSH (Time of Brute Force/Time of LSH)')
+        ax.set_ylabel('Accuracy Ratio (Similarity With LSH/With Brute Force')
     else:
-        actual[i].plot(x='Without LSH')
+        plt.scatter(x=data[i]['Without LSH'], y=data[i]['With LSH'])
+        ax = fig.add_subplot()
+        ax.set_xlabel('Without LSH')
+        ax.set_ylabel('With LSH')
+
 
 plt.show()
